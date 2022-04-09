9d48d25987e17dfee886e1c5a9a81419205459de
Minung Han <hmu332233@gmail.com>
2022-04-10 04:51:47 +0900

    썸네일 이미지 생성 로직을 html 기반으로 변경

--- pages/api/_utils/chromium.ts
+++ pages/api/_utils/chromium.ts
@@ -1,7 +1,7 @@
 import chrome from 'chrome-aws-lambda';
 import puppeteer from 'puppeteer-core';
 
-export const screenshot = async (url: string) => {
+export const screenshot = async ({ html, url }: { html?: string, url?: string }) => {
   const options = process.env.AWS_REGION
     ? {
         args: chrome.args,
@@ -16,7 +16,12 @@ export const screenshot = async (url: string) => {
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
   await page.setViewport({ width: 720, height: 480 });
-  await page.goto(url, { waitUntil: 'networkidle0' });
+  if (html) {
+    await page.setContent(html);
+  }
+  if (url) {
+    await page.goto(url, { waitUntil: 'networkidle0' });
+  }
   await page.evaluateHandle('document.fonts.ready');
   return await page.screenshot({ type: 'png' });
 }
--- pages/api/meta-image.ts
+++ pages/api/meta-image.ts
@@ -2,12 +2,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { screenshot } from './_utils/chromium';
+import { getHtml } from './_utils/template';
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const { message } = req.query;
-    const file = await screenshot(`https://celebration.minung.dev/message?hideOgImage=true&m=${encodeURI(message as string)}`);
+
+    const html = getHtml(message as string);
+    const file = await screenshot({ html });
     res.statusCode = 200;
     res.setHeader('Content-Type', `image/png`);
     res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
