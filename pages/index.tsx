11c907a2e7917b405d3800281da656a1eac358c4
Minung Han <hmu332233@gmail.com>
2022-04-10 02:11:26 +0900

    copy와 share 버튼을 분리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import useDebounce from 'hooks/useDebounce';
 import Layout from 'components/Layout';
@@ -7,8 +7,13 @@ import CopyModal from 'components/CopyModal';
 const DEFAULT_MESSAGE: string = '🎉Celebration Message🎉\nType a Message!';
 
 function Home() {
+  const [showShareButton, setShowShareButton] = useState(false);
   const [message, setMessage] = useState(DEFAULT_MESSAGE);
   const debouncedMessage = useDebounce(message, 500);
+
+  useEffect(() => {
+    setShowShareButton(typeof navigator.share === 'undefined')
+  });
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { currentTarget: { value } } = e;
@@ -16,13 +21,13 @@ function Home() {
   };
 
   const handleSubmit = async () => {
-    if (typeof navigator.share === 'undefined') {
-      await navigator.clipboard.writeText(`https://celebration.minung.dev/message?m=${encodeURI(debouncedMessage)}`);
-    } else {
-      await navigator.share({
-        url: `https://celebration.minung.dev/message?m=${encodeURI(debouncedMessage)}`,
-      });
-    }
+    await navigator.clipboard.writeText(`https://celebration.minung.dev/message?m=${encodeURI(debouncedMessage)}`);
+  };
+
+  const handleShareClick = async () => {
+    await navigator.share({
+      url: `https://celebration.minung.dev/message?m=${encodeURI(debouncedMessage)}`,
+    });
   };
 
   return (
@@ -33,6 +38,9 @@ function Home() {
           <textarea className="textarea textarea-bordered h-24" name="message" placeholder="Type in Message!" value={message} onChange={handleChange} />
         </div>
         <CopyModal.Button onClick={handleSubmit} />
+        {showShareButton && (
+          <button className="btn btn-outline btn-block mt-4" onClick={handleShareClick}>Share Link</button>
+        )}
       </div>
       <CopyModal.Modal />
     </Layout>
